ca2b26eb4879d0f8bd6ba575f1330ef4ca6979d7
Emily McLean <emilymclean93@gmail.com>
2023-05-30 16:16:24 -0400

    basic styling of project cards

--- src/components/pages/portfolio/portfolioCards.js
+++ src/components/pages/portfolio/portfolioCards.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
 export default function Portfolio() {
-    return (
-        <div className="card text-center col-lg-6">
+  return (
+    <section className='d-flex flex-column align-items-center'>
+      <div className="card text-center m-4 col-lg-6">
         <div className="card-header">
           <h2>Project goes here</h2>
         </div>
         <div className="card-body">
-            <p>Description Goes Here</p>
+          <p>Description Goes Here</p>
         </div>
       </div>
-    )
+      <div className="card text-center my-2 col-lg-6">
+        <div className="card-header">
+          <h2>Project goes here</h2>
+        </div>
+        <div className="card-body">
+          <p>Description Goes Here</p>
+        </div>
+      </div>
+      <div className="card text-center mt-2 mb-4 col-lg-6">
+        <div className="card-header">
+          <h2>Project goes here</h2>
+        </div>
+        <div className="card-body">
+          <p>Description Goes Here</p>
+        </div>
+      </div>
+    </section>
+  )
 }
