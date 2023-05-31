256d58b880ef7854ac7256a6a999a4eebac647e6
Emily McLean <emilymclean93@gmail.com>
2023-05-31 16:14:54 -0400

    moved navigation to it's own component

---
+++ src/components/Navigation.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+export default function Navigation({ currentPage, handlePageChange}) {
+    return (
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <a
+                    href="#AboutMe"
+                    onClick={() => handlePageChange('AboutMe')}
+                    className={currentPage === 'AboutMe' ? 'nav-link-active' : 'nav-link'}
+                >
+                    About Me
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link-active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link-active' : "nav-link"}
+                >Contact</a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link-active' : "nav-link"}
+                >
+                    Resume
+                </a>
+            </li>
+        </ul>
+    )
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,53 +1,18 @@
 import React from 'react';
-// import Navigation from './Navigation';
+import Navigation from './Navigation';
 
-export default function Header({ currentPage, handlePageChange }) {
+export default function Header() {
     return (
         <header className='headerStyle'>
-        <nav className="d-flex flex-wrap navbar navbar-expand-lg">
-            <a id='navbar-name' className="navbar-brand p-2" href="/">Emily McLean</a>
-            <button className="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="light navbar-toggler-icon"></span>
-            </button>
-            <div className="flex-wrap justify-content-end collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-            <li className="nav-item">
-                <a
-                    href="#AboutMe"
-                    onClick={() => handlePageChange('AboutMe')}
-                    className={currentPage === 'AboutMe' ? 'nav-link-active' : 'nav-link'}
-                >
-                    About Me
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#Portfolio"
-                    onClick={() => handlePageChange('Portfolio')}
-                    className={currentPage === 'Portfolio' ? 'nav-link-active' : 'nav-link'}
-                >
-                    Portfolio
-                </a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#Contact"
-                    onClick={() => handlePageChange('Contact')}
-                    className={currentPage === 'Contact' ? 'nav-link-active' : "nav-link"}
-                >Contact</a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#Resume"
-                    onClick={() => handlePageChange('Resume')}
-                    className={currentPage === 'Resume' ? 'nav-link-active' : "nav-link"}
-                >
-                    Resume
-                </a>
-            </li>
-        </ul>
-    </div>
-        </nav>
+            <nav className="d-flex flex-wrap navbar navbar-expand-lg">
+                <a id='navbar-name' className="navbar-brand p-2" href="/">Emily McLean</a>
+                <button className="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="light navbar-toggler-icon"></span>
+                </button>
+                <div className="flex-wrap justify-content-end collapse navbar-collapse" id="navbarNav">
+                <Navigation />
+                </div>
+            </nav>
         </header>
     );
 }
