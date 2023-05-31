8a11200dbc2a9a65ea2cb4e6dbdd5e51abed0436
Emily McLean <emilymclean93@gmail.com>
2023-05-30 23:15:58 -0400

    removed bootstrap navbar styling

--- src/components/pages/homepage/Home.js
+++ src/components/pages/homepage/Home.js
@@ -3,6 +3,7 @@ import './home.css';
 
 
 export default function Home() {
+
     return (
       <section className='h-75 d-flex flex-wrap align-content-center'>
       <div className='home-div my-5 d-flex flex-column align-items-center container'>
@@ -10,7 +11,7 @@ export default function Home() {
         <h3 className='text-center p-3 w-50'>
           I'm a Full-Stack Web Developer who loves solving problems and creating user-friendly experiences.
         </h3>
-        <span className='portfolioBtn'>Portfolio</span>
+        {/* <span className='portfolioBtn'>Portfolio</span> */}
       </div>
       </section>
     );
