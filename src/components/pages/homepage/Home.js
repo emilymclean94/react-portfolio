ca2ae83ae5c97c846f9f73d88d63785f066aba95
Emily McLean <emilymclean93@gmail.com>
2023-05-30 20:51:16 -0400

    button credit in readme and touch up styling to homepage

--- src/components/pages/homepage/Home.js
+++ src/components/pages/homepage/Home.js
@@ -7,8 +7,8 @@ export default function Home() {
       <section className='h-75 d-flex flex-wrap align-content-center'>
       <div className='home-div my-5 d-flex flex-column align-items-center container'>
         <h1 id='name' className='display-4'>Hi, I'm Emily McLean.</h1>
-        <h3 className='text-center p-3'>
-          I'm a Full Stack Web Developer! 
+        <h3 className='text-center p-3 w-50'>
+          I'm a Full-Stack Web Developer who loves solving problems and creating user-friendly experiences.
         </h3>
         <span className='portfolioBtn'>Portfolio</span>
       </div>
