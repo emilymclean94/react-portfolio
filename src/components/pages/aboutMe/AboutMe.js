d3f37afb16f7bb6a2f6a185710c6233525407fa5
Emily McLean <emilymclean93@gmail.com>
2023-05-31 10:00:54 -0400

    border radius to headshot

--- src/components/pages/aboutMe/AboutMe.js
+++ src/components/pages/aboutMe/AboutMe.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import headshot from '../../../assets/headshot.jpg';
 
 
+const styles = {
+    img: {
+    borderTopRightRadius: '150px',
+    borderTopLeftRadius: '150px',
+    },
+    aboutContainer: {
+        background: 'white',
+    }
+}
 export default function AboutMe() {
+
     return (
-        <container className='vh-100'>
-        <div className='row d-flex h-100 justify-content-center'>
-            <div className='col-lg-5 bg-light w-25 h-50 text-center'>
-                {/* <h1>Headshot</h1> */}
-                <p>Headshot here</p>
+        <container style={styles.aboutContainer}>
+            <div className='row d-flex h-75 vw-100 justify-content-center align-content-center'>
+                <div className='bg-light vw-100 d-flex justify-content-around'>
+                    <div className='col-lg-4 bg-light d-flex'>
+                        <div className='d-flex justify-content-center'>
+                            <img src={headshot} alt='headshot' className=' img-fluid mx-5 py-4' style={styles.img} />
+                        </div>
+                    </div>
+                        <div className='col-lg-7 bg-light text-center py-4'>
+                            <h1>About Me</h1>
+                            <p>Here's some text about me</p>
+                            <p>Skills icons</p>
+                        </div>
+                </div>
             </div>
-            <div className='col-lg-5 bg-light w-25 h-50 text-center'>
-                <h1>About Me</h1>
-                <p>Here's some text about me</p>
-                <p>Skills icons here</p>
-            </div>
-        </div>
         </container>
     );
 }
