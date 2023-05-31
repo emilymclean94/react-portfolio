936847b7487918b0f3c9c30153d36a92403ac9af
Emily McLean <emilymclean93@gmail.com>
2023-05-30 21:36:20 -0400

    basic structure of portfolio containers

--- src/components/pages/portfolio/portfolioCards.js
+++ src/components/pages/portfolio/portfolioCards.js
@@ -3,28 +3,30 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <section className='d-flex flex-column align-items-center'>
-      <div className="card text-center m-4 col-lg-6">
-        <div className="card-header">
-          <h2>Project goes here</h2>
+      <div className="card m-4 col-lg-8">
+        <div className="card-group">
+        <div className='card-body col-lg-1'>
+            <p>
+              aksjdhjsaflkasjflksjdlksjadsad
+            </p>
+          </div>
+        <div className='card-body col-lg-4'>
+          <h2>Event Ease</h2>
+          <p>The Event Planner is a web application built using MySQL, Node.js, and follows the MVC (Model-View-Controller) pattern. It allows users to plan and manage events, keeping track of attendees, RSVPs, and event details.</p>
         </div>
-        <div className="card-body">
-          <p>Description Goes Here</p>
-        </div>
-      </div>
-      <div className="card text-center my-2 col-lg-6">
-        <div className="card-header">
-          <h2>Project goes here</h2>
-        </div>
-        <div className="card-body">
-          <p>Description Goes Here</p>
         </div>
       </div>
-      <div className="card text-center mt-2 mb-4 col-lg-6">
-        <div className="card-header">
-          <h2>Project goes here</h2>
-        </div>
-        <div className="card-body">
-          <p>Description Goes Here</p>
+      <div className="card m-4 col-lg-8">
+        <div className="card-group">
+          <div className='card-body col-lg-1'>
+            <p>
+              aksjdhjsaflkasjflksjdlksjadsad
+            </p>
+          </div>
+          <div className='card-body col-lg-4'>
+          <h2>Player One Reads</h2>
+          <p>This is a simple web application that generates book suggestions based upon the user's video game interests. The app works when a user inputs the title of a video game and may optionally select to narrow down results by category. The app then takes both inputs and returns three book suggestions and links to Google Books.</p>
+          </div>
         </div>
       </div>
     </section>
