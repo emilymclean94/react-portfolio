03d8c84daf243bc5b6c11111be2376941807e476
Emily McLean <emilymclean93@gmail.com>
2023-05-31 21:10:30 -0400

    cleaning up code

--- src/components/pages/portfolio/portfolioCards.js
+++ src/components/pages/portfolio/portfolioCards.js
@@ -8,14 +8,9 @@ export default function PortfolioCards(props) {
       <div className="card m-4 col-lg-8">
         <div className="card-group">
         <div className='card-body col-lg-4'>
-           <img 
-           onMouseOver={() => setIsShown(true)}
-           onMouseLeave={() => setIsShown(false)}
-           src={props.projectImg} alt='project screenshot' className='img-fluid' height="25%" />
+           <img src={props.projectImg} alt='project screenshot' className='img-fluid' height="25%" />
           </div>
-          {isShown && (
-            <div>Repo Image/link (make separate component</div>
-          )}
+          
         <div className='card-body col-lg-4'>
           <h2>{props.title}</h2>
           <p> {props.description}</p>
--- src/components/pages/portfolio/projects.js
+++ src/components/pages/portfolio/projects.js
@@ -6,7 +6,7 @@ import PortfolioCards from './portfolioCards';
 export default function Projects() {
     return (
         <section className='d-flex flex-column align-items-center'>
-            <PortfolioCards projectImg={EventEase} title='Event Ease' description='The Event Planner is a web application built using MySQL, Node.js, and follows the MVC (Model-View-Controller) pattern. It allows users to plan and manage events, keeping track of attendees, RSVPs, and event details.'/>
+            <PortfolioCards projectImg={EventEase} title='Event Ease' description='This Event Planner is a web application built using MySQL, Node.js, and follows the MVC (Model-View-Controller) pattern. It allows users to plan and manage events, keeping track of attendees, RSVPs, and event details.'/>
             <PortfolioCards projectImg={PlayerOne} title='Player One Reads' description="This is a simple web application that generates book suggestions based upon the user's video game interests. The app works when a user inputs the title of a video game and may optionally select to narrow down results by category. The app then takes both inputs and returns three book suggestions and links to Google Books."/>
         </section>
     )
